dd3a3b38dc8ebfd3604c76b25d9e95581e0ec31d
ojha-sweta <ojhasweta064@gmail.com>
2026-09-13 11:51:55 +0530

    Allow Vercel frontend in CORS

--- jal-rakshak-backend/app/main.py
+++ jal-rakshak-backend/app/main.py
@@ -46,12 +46,12 @@ app.add_middleware(
     CORSMiddleware,
 
     allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1:5174",
-    ],
-
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:5174",
+    "https://jal-rakshak-pi.vercel.app",
+],
     allow_credentials=True,
 
     allow_methods=["*"],
